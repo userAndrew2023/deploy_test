fdfcd28e892ce45fee170854bfd4e60160b1dafb
Jopkins <justfredbear@gmail.com>
2024-03-14 17:23:54 +0300

    master

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from data import db_session
 if __name__ == '__main__':
     db_session.global_init('database.db')
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=80)
